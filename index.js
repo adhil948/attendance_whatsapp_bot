e366a67e9b09f6dbdc49f25dbd5a3fbaa5111689
Adhil Shahan <adhilshahanj@gmail.com>
2025-06-18 18:38:45 +0530

    updated bot logic

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const bodyParser = require("body-parser");
-require(".env").config();
+require("dotenv").config();
 const app = express();
 app.use(bodyParser.json());
 
